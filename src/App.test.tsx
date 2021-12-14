92f0c42305f08b38c9f741dfab056657fb4b3079
MehdiHan <meh.bouchard@outlook.fr>
2021-12-14 17:25:24 +0100

    chore: cleaning starter react template

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,9 +1,8 @@
-import React from 'react';
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('expect heading', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  const headerElement = screen.getByRole('heading',{level:1});
+  expect(headerElement).toBeInTheDocument();
 });
