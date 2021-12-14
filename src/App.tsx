70075c8077b9d8c50a8c694f8be8703ba75c294c
MehdiHan <meh.bouchard@outlook.fr>
2021-12-14 17:54:37 +0100

    chore: deleting default medias and adding logo svg

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,10 @@
 import './App.css';
-
+import logo from './logo_sixers.svg'
 export default function App() {
   return (
     <div className="App">
       <header className="App-header">
-      
+        <img src={logo} width="80px" height="80px" alt="Logo officiel des Philadelphia 76ers" />
       </header>
       <main className="App-main">
         <h1>Philadephia 76ers</h1>
