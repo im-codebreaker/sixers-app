af9a8ce407a6fd542cdd39347f1bee0523aad41d
MehdiHan <meh.bouchard@outlook.fr>
2022-01-03 19:51:28 +0100

    chore: adding social networks to Footer Component

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -4,10 +4,60 @@ import './Footer.scss';
 export default function Footer() {
   return (
     <footer className='App-footer'>
-      <p>
-        Copyright © 2021 - Made with <span className='heart'>♥</span> by
-        imcodebreaker
-      </p>
+      <div className='footer-wrapper container'>
+        <p className='footer-copyright'>
+          Copyright © 2021 - Made with <span className='heart'>♥</span> by
+          imcodebreaker
+        </p>
+
+        <ul className='social-networks'>
+          <li>
+            <a
+              href='https://www.nba.com/sixers/'
+              target='_blank'
+              rel='noopener noreferrer'
+            >
+              <i className='sixers'></i>
+            </a>
+          </li>
+          <li>
+            <a
+              href='https://twitter.com/Sixers'
+              target='_blank'
+              rel='noopener noreferrer'
+            >
+              <i className='twitter'></i>
+            </a>
+          </li>
+          <li>
+            <a
+              href='https://www.instagram.com/Sixers/'
+              target='_blank'
+              rel='noopener noreferrer'
+            >
+              <i className='instagram'></i>
+            </a>
+          </li>
+          <li>
+            <a
+              href='https://www.facebook.com/Sixers'
+              target='_blank'
+              rel='noopener noreferrer'
+            >
+              <i className='facebook'></i>
+            </a>
+          </li>
+          <li>
+            <a
+              href='https://www.youtube.com/user/philadelphia76ers'
+              target='_blank'
+              rel='noopener noreferrer'
+            >
+              <i className='youtube'></i>
+            </a>
+          </li>
+        </ul>
+      </div>
     </footer>
   );
 }
