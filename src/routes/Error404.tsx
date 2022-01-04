3e8c5fefe6d78c349626f3f4541bcb21f9f682b5
MehdiHan <meh.bouchard@outlook.fr>
2022-01-04 12:58:51 +0100

    chore: adding back-to-home link and display pathname

--- src/routes/Error404.tsx
+++ src/routes/Error404.tsx
@@ -1,10 +1,25 @@
 import './Error404.scss';
+import { Link } from 'react-router-dom';
 
 export default function Error404() {
+  const pathName = window.location.pathname;
+
   return (
-    <section className='error404'>
-      <h1>You are here!</h1>
-      <h2>But nothing found for you #404</h2>
+    <section className='error404 container'>
+      <div>
+        <h1>404</h1>
+      </div>
+      <div>
+        <h2>Something is missing !</h2>
+        <p className='error-message'>
+          Sorry, the following page <span className='pathname'>{pathName}</span>{' '}
+          seems to have disappeared. You should go back to the homepage until we
+          find it.
+        </p>
+        <Link to='/' className='back-homepage'>
+          Back to homepage ⮐
+        </Link>
+      </div>
     </section>
   );
 }
